03102f2e25f1bae74a22999586c6c80559742fb5
Joshi Prashant <joshiprashant.jp25@gmail.com>
2021-03-07 12:37:19 +0530

    updating downloader

--- chitChatDownloader/DownloadChitChat.java
+++ chitChatDownloader/DownloadChitChat.java
@@ -13,7 +13,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
